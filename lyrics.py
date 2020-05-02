dbdf6dae68b2b775fe956fa02fd9ba50a979b6b5
Rumeer <35002782+Rumeer@users.noreply.github.com>
2020-05-02 17:09:51 -0400

    final

--- lyrics.py
+++ lyrics.py
@@ -1,14 +1,63 @@
 import lyricsgenius
 import pandas as pd
-genius = lyricsgenius.Genius("_DXQ_ikkLyYiZENSnzV7tfbjG2_s_2wENNFY1njNCd5zNQzXDOC5oW5Dg7IxYOit")
-artist_name = "Jay Electronica"
-artist = genius.search_artist(artist_name, max_songs=3, sort="popularity")
-print(artist.songs)
+import string
+import nltk
+import random
+from nltk.corpus import stopwords
+from collections import defaultdict
 
-song = genius.search_song("The Neverending Story", artist.name)
-print(song.lyrics)
-artist.add_song(song)
-artist.save_lyrics()
+genius = lyricsgenius.Genius("WUThKvy1dkWOTqHFaNxZbt2Sa_QUfvldAmrRZnEXT2p66l24q22V36ISMmss9_JA")
+artist_name = "Joey Bada$$"
+genius.remove_section_headers = True
 
-Artist=pd.read_json("Lyrics_{artist_name}.json")
 
+
+
+def remove_punc(r):
+    """
+    this function will remove punctuation
+    """
+    punctuation = string.punctuation
+    for p in r:
+        if p in punctuation:
+            r = r.replace(p,'')
+    return r
+def lyrics_list(lyrics):
+    """
+    this will break apart the text
+    """
+    lyric = []
+    for r in lyrics.split(): 
+        r = remove_punc(r)
+        r=r.lower()
+        lyric.append(r)    
+    return lyric
+
+song_list=["GOOD MORNING AMERIKKKA","FOR MY PEOPLE","TEMPTATION","LAND OF THE FREE","DEVASTED", "AMERIKKKAN IDOL"]
+new_list = []
+for x in song_list:
+    song = genius.search_song(x, artist_name)
+    lyrics=song.lyrics
+    new_list.append(lyrics_list(lyrics))
+
+def markov_chain(new_list):
+    m_dict=defaultdict(list)
+    # print (new_list)
+    for x in range(len(new_list)):
+        for current_word,next_word in zip(new_list[x][0:-1], new_list[x][1:]):
+            m_dict[current_word].append(next_word)
+    m_dict = dict(m_dict)
+    return m_dict
+
+#print(markov_chain(new_list))
+
+def generate_sentence(m_dict,count=100):
+    wordl= random.choice(list(m_dict.keys()))
+    sentence= wordl.capitalize()
+    for i in range(count-1):
+        word2=random.choice(m_dict[wordl])
+        wordl=word2
+        sentence += ' ' + word2
+    return(sentence)
+r_dict = markov_chain(new_list)
+print(generate_sentence(r_dict))
--- lyricsv2.py
+++
@@ -1,8 +0,0 @@
-#Assign your Genius.com credentials and select your artist
-import lyricsgenius as genius
-geniusCreds = "{aWD9mHE_T2OGbXwY2kBoVjAJkojVcaYv6VvqwRHpBBcdsd60htyoExWpMdMQ7XFi}"
-artist_name = "Jay Electronica"
-
-#Connect your credentials and chosen artist to the genius object then test the first 5 songs
-api = genius.Genius(geniusCreds)
-artist = api.search_artist(artist_name, max_songs=5)
