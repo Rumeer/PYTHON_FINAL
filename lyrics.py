278632c43d8f02a5c2f9ad153181f20c2b9f1407
Rumeer <35002782+Rumeer@users.noreply.github.com>
2020-05-02 13:34:42 -0400

    final

--- lyrics.py
+++ lyrics.py
@@ -1,7 +1,7 @@
 import lyricsgenius
 import pandas as pd
 genius = lyricsgenius.Genius("_DXQ_ikkLyYiZENSnzV7tfbjG2_s_2wENNFY1njNCd5zNQzXDOC5oW5Dg7IxYOit")
-artis_name="Jay Electronica"
+artist_name = "Jay Electronica"
 artist = genius.search_artist(artist_name, max_songs=3, sort="popularity")
 print(artist.songs)
 
@@ -10,4 +10,5 @@ print(song.lyrics)
 artist.add_song(song)
 artist.save_lyrics()
 
-Artist=pd.read_json("Lyrics_{artis_name}.json")
+Artist=pd.read_json("Lyrics_{artist_name}.json")
+
