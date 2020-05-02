c079a2306029858947949900b871c2bcfaf58faf
Rumeer <35002782+Rumeer@users.noreply.github.com>
2020-05-02 12:19:56 -0400

    FINAL

---
+++ lyrics.py
@@ -0,0 +1,13 @@
+import lyricsgenius
+import pandas as pd
+genius = lyricsgenius.Genius("_DXQ_ikkLyYiZENSnzV7tfbjG2_s_2wENNFY1njNCd5zNQzXDOC5oW5Dg7IxYOit")
+artis_name="Jay Electronica"
+artist = genius.search_artist(artist_name, max_songs=3, sort="popularity")
+print(artist.songs)
+
+song = genius.search_song("The Neverending Story", artist.name)
+print(song.lyrics)
+artist.add_song(song)
+artist.save_lyrics()
+
+Artist=pd.read_json("Lyrics_{artis_name}.json")
